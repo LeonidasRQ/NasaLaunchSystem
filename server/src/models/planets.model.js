f617c59669084cec6422798e51783f4e09db38d8
Leonidas <leonidas.ramirez.q@gmail.com>
2022-03-01 21:35:00 -0500

    Populate data from csv

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,5 +1,6 @@
-const { parse } = require("csv-parse");
 const fs = require("fs");
+const path = require("path");
+const { parse } = require("csv-parse");
 
 const habitablePlanets = [];
 
@@ -14,32 +15,37 @@ function isHabitablePlanet(planet) {
 
 //The data that the below operation returns are given in Buffer format
 //Buffer: objects node uses to represent a collection of bytes
-fs.createReadStream("kepler_data.csv")
-  .pipe(
-    parse({
-      comment: "#",
-      columns: true,
-    })
-  ) //we use pipe for connecting the createReadStream output to our parse function
-  .on("data", (data) => {
-    if (isHabitablePlanet(data)) {
-      habitablePlanets.push(data);
-    }
-  })
-  .on("error", (err) => {
-    console.log(err);
-  })
-  .on("end", () => {
-    console.log(
-      habitablePlanets.map((planet) => {
-        return planet["kepler_name"];
+
+function loadPlanetsData() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(
+      path.join(__dirname, "..", "..", "data", "kepler_data.csv")
+    )
+      .pipe(
+        parse({
+          comment: "#",
+          columns: true,
+        })
+      ) //we use pipe for connecting the createReadStream output to our parse function
+      .on("data", (data) => {
+        if (isHabitablePlanet(data)) {
+          habitablePlanets.push(data);
+        }
       })
-    );
-    console.log(`${habitablePlanets.length} habitable planets found!`);
+      .on("error", (err) => {
+        console.log(err);
+        reject(err);
+      })
+      .on("end", () => {
+        console.log(`${habitablePlanets.length} habitable planets found!`);
+        resolve();
+      });
   });
+}
 
 //We want to parse every peace of data that the funtion above outputs
 //fortunately, the parse function of our third party library receives streams
 module.exports = {
+  loadPlanetsData,
   planets: habitablePlanets,
 };
--- server/src/server.js
+++ server/src/server.js
@@ -2,10 +2,17 @@ const http = require("http");
 
 const app = require("./app");
 
+const { loadPlanetsData } = require("./models/planets.model");
+
 const port = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}...`);
-});
+async function startServer() {
+  await loadPlanetsData();
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+  });
+}
+
+startServer();
