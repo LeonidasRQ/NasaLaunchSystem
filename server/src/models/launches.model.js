98273ea38bd27a65806f3e5c50796b92b18f02c5
Leonidas <leonidas.ramirez.q@gmail.com>
2022-03-02 18:12:23 -0500

    implemented launches functionalities

---
+++ server/src/models/launches.model.js
@@ -0,0 +1,18 @@
+launches = new Map();
+
+const launch = {
+  flightNumber: 100,
+  mission: "Kepler exploration X",
+  rocket: "Explorar IS1",
+  launchDate: new Date("December 27, 2030"),
+  destination: "Kepler-442 b",
+  customers: ["Nasa", "ZTM"],
+  upcoming: true,
+  success: true,
+};
+
+launches.set(launch.flightNumber, launch);
+
+module.exports = {
+  launches,
+};
