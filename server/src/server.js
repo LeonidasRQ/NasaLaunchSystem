4751ded647941cd188421e7cf2f7b777bc32e53f
Leonidas <leonidas.ramirez.q@gmail.com>
2022-03-13 15:18:38 -0500

    created database connection

--- server/src/server.js
+++ server/src/server.js
@@ -1,17 +1,31 @@
 const http = require("http");
+const mongoose = require("mongoose");
 
 const app = require("./app");
 
 const { loadPlanetsData } = require("./models/planets.model");
 
-const port = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;
+
+const MONGO_URL =
+  "mongodb+srv://nasa-api:xz7H6h8gvS8VQywq@nasacluster.v7r9q.mongodb.net/nasa?retryWrites=true&w=majority";
 
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on("error", (error) => {
+  console.error(error);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
-  app.listen(port, () => {
-    console.log(`Listening on port ${port}...`);
+
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
   });
 }
 
