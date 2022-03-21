61f1b55e007e2e08dd33923ded4359474b94ed9e
Leonidas <leonidas.ramirez.q@gmail.com>
2022-03-21 09:46:48 -0500

    implemented database flight abortion functionality

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -4,8 +4,6 @@ const planets = require("./planets.mongo");
 
 const DEFAULT_FLIGHT_NUMBER = 0;
 
-const launches = new Map();
-
 const launch = {
   flightNumber: 100,
   mission: "Kepler exploration X",
@@ -18,8 +16,10 @@ const launch = {
 };
 saveLaunch(launch);
 
-function exitsLaunchWithId(launchId) {
-  return launches.has(launchId);
+async function exitsLaunchWithId(launchId) {
+  return await launchesDB.findOne({
+    flightNumber: launchId,
+  });
 }
 
 async function getAllLaunches() {
@@ -52,7 +52,7 @@ async function saveLaunch(launch) {
     throw new Error("No matching planet was found");
   }
 
-  await launchesDB.updateOne(
+  await launchesDB.findOneAndUpdate(
     {
       flightNumber: launch.flightNumber,
     },
@@ -76,11 +76,18 @@ async function scheduleNewLaunch(launch) {
   await saveLaunch(newLaunch);
 }
 
-function abortLaunchById(launchId) {
-  const aborted = launches.get(launchId);
-  aborted.upcoming = false;
-  aborted.success = false;
-  return aborted;
+async function abortLaunchById(launchId) {
+  const aborted = await launchesDB.updateOne(
+    {
+      flightNumber: launchId,
+    },
+    {
+      upcoming: false,
+      success: false,
+    }
+  );
+
+  return aborted.modifiedCount === 1;
 }
 
 module.exports = {
