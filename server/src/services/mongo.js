92367042b5a71cfb4ca57525414d8c848918ca0a
Leonidas <leonidas.ramirez.q@gmail.com>
2022-03-24 18:36:42 -0500

    fixed security

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
-const MONGO_URL =
-  "mongodb+srv://nasa-api:xz7H6h8gvS8VQywq@nasacluster.v7r9q.mongodb.net/nasa?retryWrites=true&w=majority";
+require("dotenv").config();
+
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once("open", () => {
   console.log("MongoDB connection ready!");
